fe208e8fa8a6ea444196b32293aea05d526be39e
yuriy.kurylenko <yuriy.kurylenko@gmail.com>
2013-03-17 13:37:25 +0200

    Continuing with lazyObject

---
+++ lazyObject.js
@@ -0,0 +1,35 @@
+var lazyObject = function() {
+    var deferreds = {};
+
+    // Result returning
+    return {
+        with: function(props, callback, scope) {
+            if (_.isString(props)) {
+                props = [ props ];
+            } else if (!_.isArray(props)) {
+                throw new Error(
+                    'Error in lazyObject.with(): "property/properties list" value should be a string or an array!'
+                );
+            }
+
+            var deferredValues = _.map(props, function(propName) {
+                if (!deferreds[propName]) {
+                    deferreds[propName] = $.Deferred();
+                }
+
+                return deferreds[propName];
+            });
+
+            $.when.apply(null, deferredValues).done(function() {
+                callback.apply(scope, arguments);
+            });
+        },
+
+        set: function(prop, value) {
+            if (!deferreds[prop]) {
+                deferreds[prop] = $.Deferred();
+            }
+            deferreds[prop].resolve(value);
+        }
+    };
+}
